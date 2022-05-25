fd7f222bd34e9addf32bf80e39e6de78e0afdb66
wakeupzaman909 <hadiuzzaman909@outlook.com>
2022-05-25 17:00:10 +0600

    make admin done

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useQuery } from 'react-query';
 import { Link, Outlet } from 'react-router-dom';
+import auth from '../../firebase.init';
+import useAdmin from '../../hooks/useAdmin';
 
 const Dashboard = () => {
-
+    const [user]=useAuthState(auth);
+    const [admin]=useAdmin(user);
+    console.log(admin)
 
     return (
         <div class="drawer drawer-mobile">
@@ -15,35 +20,16 @@ const Dashboard = () => {
         <div class="drawer-side">
             <label for="dashboard-sidebar" class="drawer-overlay"></label>
             <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-                <li><Link to='/dashboard'>My Orders</Link></li>
-                <li><Link to='/dashboard/AddReview'>Add Review</Link></li>
+               { !admin &&  <li><Link to='/dashboard'>My Orders</Link></li>}
+               { !admin && <li><Link to='/dashboard/AddReview'>Add Review</Link></li>}
                 <li><Link to='/dashboard/myProfile'>My Profile</Link></li>
-                <li><Link to='/dashboard/makeAdmin'>Make Admin</Link></li>
-                <li><Link to='/dashboard/addProduct'>Add Product</Link></li>
-                <li><Link to='/dashboard/manageProduct'>Manage Product</Link></li>
-                <li><Link to='/dashboard/manageAllProduct'>Manage All Products</Link></li>
+                { admin && <li><Link to='/dashboard/makeAdmin'>Make Admin</Link></li>}
+                { admin && <li><Link to='/dashboard/addProduct'>Add Product</Link></li>}
+                { admin && <li><Link to='/dashboard/manageProduct'>Manage Product</Link></li>}
+                { admin &&  <li><Link to='/dashboard/manageAllProduct'>Manage All Products</Link></li>}
             </ul>
         </div>
     </div>
-        // <div class="drawer drawer-mobile">
-        //     <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-        //     <div class="drawer-content ">
-        //         <div className='flex justify-between'>
-        //             <h2 className='text-3xl font-bold text-indigo-400'>Dashboard</h2>
-        //             <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Dashboard</label>
-        //         </div>
-        //         <Outlet></Outlet>
-        //     </div>
-        //     <div class="drawer-side">
-        //         <label for="my-drawer-2" class="drawer-overlay"></label>
-        //         <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-        //             <li><Link to='dashboard/myOrder'>My Orders</Link></li>
-        //             <li><Link to='dashboard/addReview'>Add Review</Link></li>
-        //             <li><Link to='dashboard/myProfile'>My Profile</Link></li>
-        //         </ul>
-
-        //     </div>
-        // </div>
     );
 };
 
--- src/Pages/Dashboard/MakeAdmin.js
+++ src/Pages/Dashboard/MakeAdmin.js
@@ -6,7 +6,12 @@ import Loading from '../Shared/Loading/Loading';
 import UserRow from './UserRow';
 
 const MakeAdmin = () => {
-    const {data: users,isLoading,refetch}=useQuery('users',()=> fetch('http://localhost:5000/user').then(res=>res.json()));
+    const {data: users,isLoading,refetch}=useQuery('users',()=> fetch('http://localhost:5000/user',{
+        method: 'GET',
+        headers: {
+            'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res=>res.json()));
     
     if(isLoading){
         return <Loading></Loading>
